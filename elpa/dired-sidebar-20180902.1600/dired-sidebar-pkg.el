f2a86b63fb8d31e8436771d37d8db5fb06b25af1
Oakenhold <222205+oakenhold@users.noreply.github.com>
2018-09-09 20:33:31 +0800

    New packages
    Included dashboard, org-journal, sidebar.

---
+++ elpa/dired-sidebar-20180902.1600/dired-sidebar-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-sidebar" "20180902.1600" "Tree browser leveraging dired" '((emacs "25.1") (dired-subtree "0.0.1")) :commit "4e0c89cf99d3176809275f53571d8ca89f3f40b9" :keywords '("dired" "files" "tools") :authors '(("James Nguyen" . "james@jojojames.com")) :maintainer '("James Nguyen" . "james@jojojames.com") :url "https://github.com/jojojames/dired-sidebar")
---
+++ elpa/dired-subtree-20170910.2221/dired-subtree-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dired-subtree" "20170910.2221" "Insert subdirectories in a tree-like fashion" '((dash "2.5.0") (dired-hacks-utils "0.0.1")) :commit "b6f3b7addefa046f22a15e72a25e4368e8a33d5e" :keywords '("files") :authors '(("Matúš Goljer" . "matus.goljer@gmail.com")) :maintainer '("Matúš Goljer" . "matus.goljer@gmail.com"))
